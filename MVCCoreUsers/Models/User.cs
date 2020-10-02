3ebee0e93364ffaad1f01176c050d552cbe82e28
Gerardo <Villalobos>
2020-10-02 14:17:11 -0500

    Integración de View, modificación a /Shared/_Layout y paso de data de controller a view

---
+++ MVCCoreUsers/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreUsers.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string Nick { get; set; }
+
+        public string Password { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreateDate { get; set; }
+    }
+}
