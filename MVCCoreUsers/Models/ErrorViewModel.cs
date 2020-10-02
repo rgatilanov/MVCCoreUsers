c4a5e9a4292f1b49073c0f6e1ca0e6a01f858c79
Gerardo <Villalobos>
2020-10-02 13:50:44 -0500

    Commit original del repositorio pasado

---
+++ MVCCoreUsers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCCoreUsers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
