0a6368e0918dec90143dc9e92e333c5a290f02e0
Gerardo <Villalobos>
2020-10-02 14:57:09 -0500

    Integración de Elemento con Andamiaje, creación de nuevo controller "users" y creación de bd con "Add-Migration Initial" y "Update-Database"

---
+++ MVCCoreUsers/Data/MVCCoreUsersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCCoreUsers.Models;
+
+namespace MVCCoreUsers.Data
+{
+    public class MVCCoreUsersContext : DbContext
+    {
+        public MVCCoreUsersContext (DbContextOptions<MVCCoreUsersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCCoreUsers.Models.User> User { get; set; }
+    }
+}
--- MVCCoreUsers/Startup.cs
+++ MVCCoreUsers/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MVCCoreUsers.Data;
 
 namespace MVCCoreUsers
 {
@@ -24,6 +26,9 @@ namespace MVCCoreUsers
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MVCCoreUsersContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MVCCoreUsersContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
